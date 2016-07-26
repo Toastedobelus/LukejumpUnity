4fdcdf3f0a28d572cf30b92b2f1c7dfb562f2694
Toastedobelus <Toasted.Relief@gmail.com>
2016-07-26 16:19:01 +1200

    InitialChanges
    initial local

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    private Rigidbody2D PlayerRigidBody;
+    public float JumpForce = 250f;
+
+	// Use this for initialization
+	void Start () {
+        PlayerRigidBody = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonUp("Jump"))
+        {
+            PlayerRigidBody.AddForce(transform.up * JumpForce);
+        }
+	
+	}
+}
