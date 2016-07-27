35e3ef5706a89910898a37655ef525026113b082
Toastedobelus <Toasted.Relief@gmail.com>
2016-07-27 14:46:50 +1200

    ver0.02
    refined movement.

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -3,20 +3,53 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D PlayerRigidBody;
-    public float JumpForce = 250f;
+    private Animator PlayerAnimator;
+    private SpriteRenderer PlayerSprite;
+    public float JumpForce;
+    public float MoveSpeed;
 
 	// Use this for initialization
 	void Start () {
         PlayerRigidBody = GetComponent<Rigidbody2D>();
-	
+        PlayerAnimator = GetComponent<Animator>();
+        PlayerSprite = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonUp("Jump"))
+
+        PlayerAnimator.SetBool("Hcheck", false);
+        if (Input.GetButtonDown("Jump"))
         {
             PlayerRigidBody.AddForce(transform.up * JumpForce);
         }
-	
-	}
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.position += Vector3.left * MoveSpeed * Time.deltaTime;
+            PlayerAnimator.SetBool("Hcheck", true);
+            PlayerSprite.flipX = false;
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.position += Vector3.right * MoveSpeed * Time.deltaTime;
+            PlayerAnimator.SetBool("Hcheck", true);
+            PlayerSprite.flipX = true;
+        }
+
+        //Animation Checks 
+        PlayerAnimator.SetFloat("Vspeed", PlayerRigidBody.velocity.y);
+
+        if (PlayerRigidBody.velocity.y != 0)
+        {
+            PlayerAnimator.SetBool("Vcheck", true);
+        }
+        else
+        {
+            PlayerAnimator.SetBool("Vcheck", false);
+        }
+
+    
+
+
+    }
 }
